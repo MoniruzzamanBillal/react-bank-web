1d88b0f867036833c23179feae490c6d57434fc6
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2023-08-14 23:41:37 +0600

    hero done with responsive

--- src/Components/GetStarted.jsx
+++ src/Components/GetStarted.jsx
@@ -5,18 +5,18 @@ import { arrowUp } from "../assets";
 const GetStarted = () => {
   return (
     <>
-      <div className="getContainer w-[7rem] h-[7rem] bg-blue-gradient p-1 rounded-full  ">
+      <div className="getContainer w-[4rem] h-[4rem]  md:w-[5rem] md:h-[5rem]  lg:w-[6rem] lg:h-[6rem] xl:w-[7rem] xl:h-[7rem] bg-blue-gradient p-1 rounded-full  ">
         <div className="getWrapper w-[100%] h-[100%] m-auto bg-[#00040F] rounded-full flex flex-col justify-center items-center text-white ">
           <div className="itemContainer">
             {/*  */}
-            <div className="div1 flex text-gradient mb-1">
+            <div className="div1 flex text-gradient mb-1  text-xs md:text-base ">
               <h1>get </h1>
-              <img src={arrowUp} alt="" />
+              <img src={arrowUp} alt="" className=" w-[34%] md:w-full  " />
             </div>
             {/*  */}
 
             {/*  */}
-            <div className="div2 text-gradient">
+            <div className="div2 text-gradient  text-xs md:text-base  ">
               <h1> Started </h1>
             </div>
             {/*  */}
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,38 +9,49 @@ const Hero = () => {
       <div className="heroContainer ">
         <div className="heroWrapper  flex ">
           {/* left side starts  */}
-          <div className="heroLeftContainer  w-[60%] flex flex-col justify-center">
-            <div className="heroLeftContainerWrapper  ml-20 relative">
+          <div className="heroLeftContainer w-[52%] sm:w-[60%] flex flex-col justify-center">
+            <div className="heroLeftContainerWrapper ml-10 sm:ml-12 md:ml-16 lg:ml-20 relative  ">
               {/* left top section  */}
-              <div className="heroLeftTop  mb-6">
+              <div className="heroLeftTop inline-block mb-2 sm:mb-4 md:mb-5 lg:mb-6">
                 {/* hero top left starts  */}
-                <div className="heroLeftTopContainer bg-discount-gradient  flex items-center self-center px-3 py-1 w-[50%] text-start">
-                  <div className="heroLeftTopLogo  mr-2">
-                    <img src={discount} alt="" />
+                <div className="heroLeftTopContainer bg-discount-gradient  flex items-center self-center px-3 py-1 w-[100%] justify-start text-center ">
+                  <div className="heroLeftTopLogo mr-[.1rem] sm:mr-[.15rem] md:mr-1 lg:mr-2  hidden sm:block">
+                    <img
+                      src={discount}
+                      alt=""
+                      className=" w-[78%] sm:w-[84%] md:w-[87%] lg:w-[100%]  "
+                    />
                   </div>
 
-                  <div className="heroLeftTopPara  text-[#FFFFFF]">
+                  <div className="heroLeftTopPara text-[#FFFFFF] text-[.65rem] sm:text-xs md:text-sm lg:text-base ">
                     <p>
                       20% <span className="text-[#FFFFFF99]">DISCOUNT FOR</span>{" "}
                       1 MONTH <span className="text-[#FFFFFF99]">ACCOUNT</span>
                     </p>
                   </div>
                 </div>
+
                 {/* hero top left ends */}
               </div>
 
               {/* middle side of hero section */}
               <div className="heroLeftMid  text-white">
-                <div className="heroMidHeading text-7xl font-semibold">
-                  <h1 className="mb-5">The Next</h1>
-                  <h1 className="mb-5 text-gradient">Generation</h1>
-                  <h1 className="mb-9"> Payment Method.</h1>
+                <div className="heroMidHeading text-xl sm:text-2xl md:text-4xl lg:text-5xl  xl:text-7xl font-semibold">
+                  <h1 className=" mb-[.01rem] sm:mb-1 md:mb-2 lg:mb-4 xl:mb-5 ">
+                    The Next
+                  </h1>
+                  <h1 className=" mb-[.01rem] sm:mb-1 md:mb-2 lg:mb-4 xl:mb-5 text-gradient">
+                    Generation
+                  </h1>
+                  <h1 className=" text-base sm:text-2xl md:text-4xl lg:text-5xl  xl:text-7xl mb-1 sm:mb-2 md:mb-3 lg:mb-4 xl:mb-5">
+                    Payment Method.
+                  </h1>
                 </div>
               </div>
 
               {/* bottom side of hero section  */}
-              <div className="heroLeftBottom  w-[65%] mt-6">
-                <p className="text-[#FFFFFFB2] text-lg leading-8">
+              <div className="heroLeftBottom w-[100%] sm:w-[90%] md:w-[84%] lg:w-[70%] mt-3 md:mt-5 lg:mt-6  ">
+                <p className="text-[#FFFFFFB2] text-xs sm:text-[.8rem] md:text-base lg:text-lg leading-5 sm:leading-6 md:leading-7 lg:leading-8">
                   Our team of experts uses a methodology to identify the credit
                   cards most likely to fit your needs. We examine annual
                   percentage rates, annual fees.
@@ -48,17 +59,17 @@ const Hero = () => {
               </div>
 
               {/* get started container  */}
-
-              <div className="getStartedContainer absolute top-[30%] right-[20%] transform -translate-x-1/2 -translate-y-1/2">
+              <div className="getStartedContainer absolute  transform translate-x-1/2 translate-y-1/2 top-3   right-32  hidden sm:block  ">
                 <GetStarted />
               </div>
               {/* get started container ends */}
             </div>
+            {/*  */}
           </div>
           {/* left side ends  */}
 
           {/* right side starts  */}
-          <div className="heroRightContainer w-[40%] ">
+          <div className="heroRightContainer w-[48%] sm:w-[40%] ">
             <div className="heroRightContainerWrapper  relative ">
               {/*  */}
               <div className="heroRightImg    ">
