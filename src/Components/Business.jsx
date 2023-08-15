1cfd881462db163325a0d283d4ad607bc3f83747
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2023-08-15 22:19:57 +0600

    business done with responsiveness

--- src/Components/Business.jsx
+++ src/Components/Business.jsx
@@ -8,20 +8,20 @@ import Button from "./Button";
 const Business = () => {
   return (
     <>
-      <div className="businessContainer bg-red-400">
-        <div className="businessWrapper bg-green-400 w-[85%] m-auto flex justify-between   ">
+      <div className="businessContainer  mb-6">
+        <div className="businessWrapper  w-[85%] m-auto flex flex-col md:flex-row justify-between   ">
           {/*  */}
           {/* left side starts  */}
-          <div className="businessLeft bg-blue-400 w-[52%] ">
+          <div className="businessLeft  w-[100%] md:w-[51%] mb-5 md:mb-0 ">
             <div className="businessLeftWrapper">
               {/* top side of container starts  */}
-              <div className="businessLeftTop bg-lime-400 text-white text-5xl font-semibold mb-5 ">
+              <div className="businessLeftTop text-white  font-semibold mb-4 md:mb-7 text-4xl lg:text-5xl ">
                 <h1>You do the business, we’ll handle the money.</h1>
               </div>
               {/* top side of container ends  */}
 
               {/* middle container starts  */}
-              <div className="businessLeftMid bg-green-400 text-lg text-[#FFFFFFB2] mb-5 ">
+              <div className="businessLeftMid text-base lg:text-lg text-[#FFFFFFB2] mb-4 md:mb-7 ">
                 <p>
                   With the right credit card, you can improve your financial
                   life by building credit, earning rewards and saving money. But
@@ -31,10 +31,10 @@ const Business = () => {
               {/* middle container ends  */}
 
               {/* bottom container starts  */}
-              <div className="businessLeftBottom bg-violet-500 ">
+              <div className="businessLeftBottom  ">
                 <button
                   type="button"
-                  class="py-2.5 px-5 mr-2 mb-2 text-base font-medium text-gray-900 outline-none  rounded border border-gray-200 hover:bg-gray-100 hover:text-blue-700 active:scale-95   bg-gradient-to-br from-teal-200 via-cyan-300 to-blue-500  "
+                  class="py-2.5 px-5 text-base font-medium text-gray-900 outline-none rounded hover:text-white active:scale-95 bg-gradient-to-br from-teal-200 via-cyan-300 to-blue-500 "
                 >
                   Get Started
                 </button>
@@ -49,8 +49,49 @@ const Business = () => {
           {/*  */}
 
           {/* right side starts  */}
-          <div className="businessRight bg-amber-200 w-[48%] ">
-            right side of business
+          <div className="businessRight m-auto w-[100%] sm:w-[90%] md:w-[48%] flex flex-col justify-center ">
+            <div className="businessRightContainer   ">
+              {features.map((ele, ind) => (
+                <>
+                  <div
+                    className={`businessRightContainerWrapper  flex items-center justify-between cursor-pointer rounded feature-card py-1  ${
+                      ind === features.length - 1 ? "mb-0" : "mb-3"
+                    }`}
+                  >
+                    {/* left side card  */}
+                    <div className="businessRightLeft  w-[10%] m-auto flex justify-center items-center self-center   ">
+                      <div className="logoLeftContainer bg-[#09977C1A] p-2 rounded-full">
+                        <img
+                          src={ele.icon}
+                          alt=""
+                          className=" w-[78%] h-[78%] sm:w-[85%] sm:h-[85%] md:w-[75%] md:h-[75%]  lg:w-[80%] lg:h-[80%] m-auto   "
+                        />
+                      </div>
+                    </div>
+                    {/*  */}
+
+                    {/* right side of card  */}
+                    <div className="businessRightRight  w-[88%]  ">
+                      <div className="businessRightCardWrapper ">
+                        {/*  */}
+                        <div className="RightCardWrapperLeft  mb-1 text-[#FFFFFF] font-semibold text-sm sm:text-base md:text-sm lg:text-base ">
+                          {ele.title}
+                        </div>
+                        {/*  */}
+
+                        {/*  */}
+                        <div className="RightCardWrapperRight text-[#FFFFFFB2] text-sm sm:text-base md:text-sm lg:text-base ">
+                          {ele.content}
+                        </div>
+
+                        {/*  */}
+                      </div>
+                    </div>
+                    {/*  */}
+                  </div>
+                </>
+              ))}
+            </div>
           </div>
           {/* right side ends  */}
         </div>
--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -6,15 +6,13 @@ import styles from "../Style";
 const Stats = () => {
   return (
     <>
-      <div className="statsContainer  mb-6">
+      <div className="statsContainer  mb-12">
         <div className="startsWrapper  w-[85%] m-auto flex justify-between  items-center p-2 flex-col sm:flex-row ">
           {stats.map((ele, ind) => (
             <div
-              className={`statItem w-[90%]  sm:w-[33%]  flex justify-center items-center ${
-                ind === stats.length - 1
-                  ? "sm:border-r-0 mb-0"
-                  : "sm:border-r-2 mb-4"
-              } border-gray-400 sm:mb-0 `}
+              className={`statItem w-[90%]  sm:w-[33%] flex justify-center items-center ${
+                ind === stats.length - 1 ? " mb-0" : " mb-4"
+              }  sm:mb-0 `}
               key={ele.id}
             >
               <h4 className=" text-white pr-2 text-4xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold ">
