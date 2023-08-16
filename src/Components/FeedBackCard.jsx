fe73b71e2ba23a02b2fa075e50cbd2c6ce9226c6
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2023-08-16 18:35:03 +0600

    testimonial done

--- src/Components/FeedBackCard.jsx
+++ src/Components/FeedBackCard.jsx
@@ -8,20 +8,24 @@ const FeedBackCard = ({ content, name, title, img }) => {
 
   return (
     <>
-      <div className="feedbackContainer bg-violet-400  ">
-        <div className="feedbackWrapper bg-orange-300 w-[100%] md:min-h-[19rem]  lg:min-h-[18rem] m-auto flex flex-col  justify-center  feedback-card rounded-md ">
-          <div className="feedBackCardContent px-6 md:px-3 lg:px-5 py-10 md:py-1   ">
+      <div className="feedbackContainer  ">
+        <div className="feedbackWrapper w-[100%] md:min-h-[19rem]  lg:min-h-[18rem] m-auto flex flex-col  justify-center  feedback-card rounded-md ">
+          <div className="feedBackCardContent px-7 md:px-4 lg:px-6 py-10 md:py-1   ">
             {/*  */}
             {/* top section of feedback card starts  */}
-            <div className="feedbackCardTop bg-red-400 mb-4 w-[2.2rem] h-[2.2rem]  ">
-              <img src={quotes} alt="" className=" w-[90%] h-[90%]  " />
+            <div className="feedbackCardTop mb-4 w-[1.9rem] h-[1.9rem] sm:w-[2.2rem] sm:h-[2.2rem]  ">
+              <img
+                src={quotes}
+                alt=""
+                className=" w-[80%] h-[80%] sm:w-[90%] sm:h-[90%]  "
+              />
             </div>
             {/* top section of feedback card ends  */}
             {/*  */}
 
             {/*  */}
             {/* mid section of feedback card starts  */}
-            <div className="feedbackCardMid bg-blue-500 mb-4 w-[100%] md:w-[98%] lg:w-[90%]    ">
+            <div className="feedbackCardMid mb-4 w-[100%] md:w-[98%] lg:w-[90%] text-sm sm:text-base text-[#FFFFFFB2]   ">
               <p>{content}</p>
             </div>
             {/* mid section of feedback card ends  */}
@@ -30,12 +34,12 @@ const FeedBackCard = ({ content, name, title, img }) => {
             {/*  */}
             {/* bottom  section of feedback card starts  */}
 
-            <div className="feedbackCardBottom bg-green-500 flex justify-between items-center self-center    ">
+            <div className="feedbackCardBottom flex justify-between items-center self-center    ">
               {/* left side of card bottom starts  */}
-              <div className="feedbackCardBottomLeft bg-emerald-300 w-[15%] flex justify-center items-center self-center ">
+              <div className="feedbackCardBottomLeft  w-[15%] flex justify-center items-center self-center  pr-1 sm:pr-0 ">
                 {" "}
                 <img
-                  class="w-12 h-12 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+                  class="w-11 h-11  sm:w-12 sm:h-12 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 "
                   src={img}
                   alt="Bordered avatar"
                 />{" "}
@@ -43,15 +47,15 @@ const FeedBackCard = ({ content, name, title, img }) => {
               {/* left side of card bottom ends   */}
 
               {/* right side of card bottom starts  */}
-              <div className="feedbackCardBottomRight bg-pink-500 w-[83%]  ">
+              <div className="feedbackCardBottomRight w-[83%]  ">
                 {/*  */}
-                <div className="feedbackBottomRightLeft bg-blue-400 ">
+                <div className="feedbackBottomRightLeft text-sm sm:text-base text-white ">
                   <p>{name}</p>
                 </div>
                 {/*  */}
 
                 {/*  */}
-                <div className="feedbackBottomRightRight bg-yellow-400 text-[#FFFFFFB2] ">
+                <div className="feedbackBottomRightRight text-[#FFFFFFB2] text-sm sm:text-base ">
                   <p> {title} </p>
                 </div>
                 {/*  */}
--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -7,25 +7,24 @@ import FeedBackCard from "./FeedBackCard";
 const Testimonials = () => {
   return (
     <>
-      <div className="testimonialWrapper bg-violet-500 mb-8 sm:mb-20  ">
-        <div className="testimonialWrapper bg-green-500 w-[85%] m-auto  ">
+      <div className="testimonialWrapper  mb-8 sm:mb-20 ">
+        <div className="testimonialWrapper  w-[85%] m-auto ">
           {/* testimonial top section starts  */}
 
-          <div className="testimonialTop mb-8 flex justify-between items-center self-center   ">
+          <div className="testimonialTop mb-4 sm:mb-6 md:mb-8 flex justify-between items-center self-center   ">
             {/*  */}
-            <div className="testimonialTopLeft bg-red-600 w-[38%] p-1 text-white text-5xl ">
+            <div className="testimonialTopLeft w-[49%] md:w-[47%] lg:w-[44%] p-1 text-white text-3xl md:text-4xl lg:text-5xl ">
               <h1>What people are saying about us</h1>
             </div>
             {/*  */}
 
             {/*  */}
-            <div className="testimonialTopRight bg-blue-600 w-[44%] pr-8 text-[#FFFFFFB2] text-lg ">
+            <div className="testimonialTopRight  w-[50%] md:w-[45%] md:pr-6 lg:pr-8 text-[#FFFFFFB2] text-sm sm:text-base md:text-lg">
               <p>
                 Everything you need to accept card payments and grow your
                 business anywhere on the planet.
               </p>
             </div>
-            {/*  */}
 
             {/*  */}
           </div>
@@ -34,12 +33,12 @@ const Testimonials = () => {
 
           {/* testimonial bottom section starts  */}
 
-          <div className="testimonialWrapper bg-violet-500 mb-8 sm:mb-20">
+          <div className="testimonialWrapper  mb-8 sm:mb-20">
             {/* ... */}
-            <div className="testimonialBottom flex flex-col md:flex-row bg-blue-700 ">
+            <div className="testimonialBottom flex flex-col md:flex-row ">
               {feedback &&
                 feedback.map((ele, ind) => (
-                  <div className="itemsCard mb-6 md:mb-0 w-[68%] md:w-[32%] m-auto bg-red-700 ">
+                  <div className="itemsCard mb-6 md:mb-0 w-[86%] sm:w-[65%] md:w-[32%] m-auto  ">
                     <FeedBackCard {...ele} key={ind} />
                   </div>
                 ))}
